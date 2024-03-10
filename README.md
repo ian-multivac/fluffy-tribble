94b3e5573945b5bd329af22ef69d472ee16a2647
ian-multivac <71193863+ian-multivac@users.noreply.github.com>
2024-03-10 15:09:02 -0300

    Updates README and docstrings

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# fluffy-tribble
-Streamlit sandbox
+# Weather Stations
+
+This Streamlit app fetches and displays weather conditions from Canadian weather stations.  It uses the [env-canada](https://pypi.org/project/env-canada/) to retrieve current and historical weather conditions from [Environment Canada's repositories](https://dd.weather.gc.ca).
+
+## Getting Started
+
+Set up a virtual environment and activate it:
+
+```bash
+python3 -m venv env
+source env/bin/activate
+```
+
+Then, install the required libraries:
+
+```bash
+pip install -r requirements.txt
+```
+
+To run the app, use
+
+```bash
+streamlit run weather_stations.py
+```
--- weather_stations.py
+++ weather_stations.py
@@ -13,7 +13,12 @@ WEATHER_SITES_URL = 'https://dd.weather.gc.ca/citypage_weather/docs/site_list_pr
 WEATHER_URL = 'https://dd.weather.gc.ca/citypage_weather/xml/PE/s0000026_e.xml'
 
 @st.cache_data
-def load_data():
+def load_sites_data():
+    """Loads weather sites from Environment Canada's most recent sites list
+
+    Returns:
+        DataFrame: a pandas DataFrame containing site codes, names, and lat/long
+    """
     data = pd.read_csv(WEATHER_SITES_URL, header=1)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
@@ -25,13 +30,30 @@ def load_data():
     return data
 
 
-def split_date_range(range_string):
-    
-    return pd.Series(range_string.split('|', 1))
+def split_date_range(range_col):
+    """Splits a column that contains a `start_date | end_date` string into separate columns
 
+    Args:
+        range_col (Series): A DataFrame column to split
+
+    Returns:
+        Series: Two new columns with dates separated
+    """
+    
+    return pd.Series(range_col.split('|', 1))
 
 
 def process_station_dates(df):
+    """Accepts a DataFrame containing `hlyRange`, `dlyRange` and `mlyRange` columns, and splits
+    them into `<period>_data_start`, `<period>_data_end` formats.  Removes original range data.
+    Converts dates to datetime columns.
+
+    Args:
+        df (DataFrame): A DataFrame containing `hlyRange`, `dlyRange`, `mlyRange`
+
+    Returns:
+        DataFrame: Returns a transformed DataFrame with separated start / end columns, as datetimes.
+    """
     df = df.T
     df[['hourly_data_start', 'hourly_data_end']] = df['hlyRange'].apply(split_date_range)
     df[['daily_data_start', 'daily_data_end']] = df['dlyRange'].apply(split_date_range)
@@ -55,7 +77,15 @@ def process_station_dates(df):
 
 
 def choose_historical_station_id(df):
-    # filter only those that have daily
+    """Parses historical station data to retrieve the closest station that has recent daily data.
+
+    Args:
+        df (DataFrame): Historical weather station data, as output from process_station_dates
+
+    Returns:
+        df: A single station row that contains the most recent daily data
+    """
+    # filter only those that have daily data
     selected_stations = df[df['daily_data_end'] == dt.datetime.strftime((dt.datetime.today() - dt.timedelta(days=1)), format='%Y-%m-%d')]
     selected_stations = selected_stations.sort_values(by='proximity', ascending=True)
     selected_station = selected_stations['id'].iloc[0]
@@ -64,6 +94,14 @@ def choose_historical_station_id(df):
 
     
 def get_conditions(station_id):
+    """Retrieves the latest weather conditions from a given weather station.
+
+    Args:
+        station_id (String): Must be in the format `Province Code`/`Station Code`.
+
+    Returns:
+        dict: Dictionary containing the latest weather conditions.
+    """
     # station_id='ON/s0000430'
     weather = ECWeather(station_id=station_id, language='english')
     asyncio.run(weather.update())
@@ -72,6 +110,14 @@ def get_conditions(station_id):
 
 
 def format_conditions_data(conditions_data):
+    """Transforms DataFrame for readability.
+
+    Args:
+        conditions_data (DataFrame): Weather conditions data as output by get_conditions
+
+    Returns:
+        df: A DataFrame with reordered columns for readability
+    """
     
     df = pd.DataFrame()
     df['label'] = conditions_data['label']
@@ -82,27 +128,50 @@ def format_conditions_data(conditions_data):
     
     
 def display_conditions(station):
+    """Retrieves current weather conditions from a given station site.
+
+    Args:
+        station (DataFrame): A single station site row, as output from load_sites_data
+    """
     
     station_id = f"{station['province codes'].values[0]}/{station['codes'].values[0]}"
     conditions = get_conditions(station_id)
     conditions_data = pd.DataFrame.from_dict(conditions).T
-    
     conditions_data = format_conditions_data(conditions_data)
     
+    # Update the session state
     st.session_state.conditions = conditions_data
 
     
 def display_historical(station_id):
+    """Retrieves historical weather data from a given station site row, as output from load_sites_data.
+    Updates display, including station site metadata.
+
+    Args:
+        station_id (DataFrame): A single station site row, as output from load_sites_data
+    """
     station_coords = (station['latitude'].values[0], station['longitude'].values[0])
     station_id = lookup_stations(station_coords, radius=25, limit=10)
-    metadata, st.session_state.history = get_historical_data(station_id)
+    metadata, history = get_historical_data(station_id)
     
+    # Update session state
+    st.session_state.history = history
     st.session_state.stn_name = metadata['name']
     st.session_state.stn_identifier = metadata['climate_identifier']
     st.session_state.stn_location = (float(metadata['latitude']), float(metadata['longitude']))
 
 
 def lookup_stations(coordinates, radius, limit):
+    """Retrieves the closest weather station site to the provided coordinates that has daily data.
+
+    Args:
+        coordinates (tuple): Latitude and longitude, formatted as a tuple
+        radius (int): Distances in kilometers to search, from the center of the coordinates provided
+        limit (int): The total number of records to return, value one of [10, 25, 50, 100]
+
+    Returns:
+        int: The historical station ID for the retrieved station.
+    """
     stations = asyncio.run(get_historical_stations(coordinates, radius=radius, limit=limit))
     stations_df = pd.DataFrame.from_dict(stations)
     stations_df = process_station_dates(stations_df)
@@ -112,6 +181,14 @@ def lookup_stations(coordinates, radius, limit):
 
     
 def get_historical_data(station_id):
+    """Retrieves historical station metadata and weather data from a given historical station ID.
+
+    Args:
+        station_id (int): The historical station ID as provided by lookup_stations
+
+    Returns:
+        _type_: _description_
+    """
     ec_en_csv = ECHistorical(station_id=station_id, year=2024, language='english', format='csv')
     asyncio.run(ec_en_csv.update())
     
@@ -122,33 +199,40 @@ def get_historical_data(station_id):
     
     
 def update_displays(station):
+    """Updates data tables in main app
+
+    Args:
+        station (DataFrame): A single DataFrame row that contains weather station information
+        provided by load_sites_data.
+    """
     
     display_conditions(station)
     display_historical(station)
 
 
+
+### Main
+
+
 st.sidebar.header('Canadian Weather Stations')
 
 with st.spinner('Loading data...'):
-# Create a text element and let the user know the data is loading.
+# Load available WEATHER_SITES
     data_load_state = st.text('Loading data...')
-    # Load 10,000 rows of data into the dataframe.
-    station_data = load_data()
-    # Notify the user that the data was successfully loaded.
+    station_data = load_sites_data()
     data_load_state.text('')
 
 
-provs = ['AB', 'BC', 'MB', 'NB', 'NL', 'NS', 'NT', 'NU', 'ON', 'PE', 'QC', 'SK', 'YT']
+provinces = ['AB', 'BC', 'MB', 'NB', 'NL', 'NS', 'NT', 'NU', 'ON', 'PE', 'QC', 'SK', 'YT']
 
-prov_to_filter = st.sidebar.selectbox('Choose a province', provs)
-filtered_data = station_data[station_data['province codes'] == prov_to_filter]
+province_to_filter = st.sidebar.selectbox('Choose a province', provinces)
+filtered_data = station_data[station_data['province codes'] == province_to_filter]
 
-st.subheader(f'Locations of all stations in {prov_to_filter}')
+st.subheader(f'Locations of all stations in {province_to_filter}')
 st.map(filtered_data)
 
-stations_to_filter = st.sidebar.selectbox(f'Choose a station in {prov_to_filter}', filtered_data['english names'])
+stations_to_filter = st.sidebar.selectbox(f'Choose a station in {province_to_filter}', filtered_data['english names'])
 station = filtered_data[filtered_data['english names'] == stations_to_filter]
-
 
 if 'conditions' not in st.session_state:
     st.session_state.conditions = pd.DataFrame()
@@ -185,8 +269,3 @@ history_output_df = st.dataframe(st.session_state.history)
 st.sidebar.button('Refresh Data',
                   on_click=update_displays(station),
                   )
-
-
-
-
-
